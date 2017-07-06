da7c16bc89cc634965e6927ca9cb71f5a7abcfa1
kantorgong <kantorgong@163.com>
2017-07-06 11:31:44 +0800

    增加index.php

---
+++ backend/web/index.php
@@ -0,0 +1,18 @@
+<?php
+ini_set("session.cookie_httponly", 1);
+defined('YII_DEBUG') or define('YII_DEBUG', true);//生产后本行注释掉
+defined('YII_ENV') or define('YII_ENV', 'dev');//测试环境 'dev' 修改为 'test', 生产环境 'dev' 修改为 'prod'
+require(__DIR__ . '/../../vendor/autoload.php');
+require(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
+require(__DIR__ . '/../../common/config/bootstrap.php');
+require(__DIR__ . '/../config/bootstrap.php');
+
+$config = yii\helpers\ArrayHelper::merge(
+    require(__DIR__ . '/../../common/config/main.php'),
+    require(__DIR__ . '/../../common/config/main-local.php'),
+    require(__DIR__ . '/../config/main.php'),
+    require(__DIR__ . '/../config/main-local.php')
+);
+
+$application = new yii\web\Application($config);
+$application->run();
